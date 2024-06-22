88f2c5b1ee7a62d096286c8a787d63aae0cde2d0
jayeen28 <jayeen6783@gmail.com>
2024-06-22 20:38:27 +0600

    simple problem solved

---
+++ vjudge/yesNo.c
@@ -0,0 +1,13 @@
+// Problem link: https://vjudge.net/contest/566680#problem/I
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if (a < b && b < c)
+        printf("Yes\n");
+    else
+        printf("No\n");
+    return 0;
+}
