9bdb0f638343b5a8afbd7fb800aa220438a8e141
jayeen28 <jayeen6783@gmail.com>
2024-06-22 21:03:57 +0600

    simple problem fixed

---
+++ vjudge/s_l_e.c
@@ -0,0 +1,18 @@
+// Problem link: https://vjudge.net/contest/566680#problem/J
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if (a > b)
+        printf("a > b\n");
+    else if (a < b)
+        printf("a < b\n");
+    else if (a == b)
+        printf("a == b\n");
+
+    return 0;
+}
