a15476e8fa88fd8269e5835974d0f5d3497c9489
jayeen28 <jayeen6783@gmail.com>
2024-06-22 23:27:26 +0600

    better solution added for abc235 problem

--- vjudge/abc235_a.c
+++ vjudge/abc235_a.c
@@ -4,40 +4,14 @@
 
 int main()
 {
-    int pattern[3][3] = {{0, 1, 2}, {1, 2, 0}, {2, 0, 1}};
-    char input[3];
-    scanf("%s", input);
-    char x[4], y[4], z[4];
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            switch (i)
-            {
-            case 0:
-                // printf("x i%d j%d p%d %c\n", i, j, pattern[i][j], input[pattern[i][j]]);
-                x[j] = input[pattern[i][j]];
-                if (j == 2)
-                    x[3] = '\0';
-                break;
-            case 1:
-                // printf("y i%d j%d p%d %c\n", i, j, pattern[i][j], input[pattern[i][j]]);
-                y[j] = input[pattern[i][j]];
-                if (j == 2)
-                    y[3] = '\0';
-                break;
-            case 2:
-                // printf("z i%d j%d p%d %c\n", i, j, pattern[i][j], input[pattern[i][j]]);
-                z[j] = input[pattern[i][j]];
-                if (j == 2)
-                    z[3] = '\0';
-                break;
-            default:
-                break;
-            }
-        }
-    };
-    int sum = atoi(x) + atoi(y) + atoi(z);
-    printf("%d\n", sum);
+    int input, mid_dec, c, a, b, x, y, z;
+    scanf("%d", &input);
+    c = input % 10;
+    b = (input / 10) % 10;
+    a = (input / 100) % 10;
+    x = ((a * 10) + b) * 10 + c;
+    y = ((b * 10) + c) * 10 + a;
+    z = ((c * 10) + a) * 10 + b;
+    printf("%d\n", x + y + z);
     return 0;
 }
