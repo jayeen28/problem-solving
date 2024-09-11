24724f786ddfe907b8386f6dae0e2f1bf6580000
jayeen28 <jayeen6783@gmail.com>
2024-09-11 18:56:59 +0600

    light oj greeting problem solved

---
+++ lightoj/greetings.c
@@ -0,0 +1,17 @@
+// problem link: https://lightoj.com/problem/greetings-from-lightoj
+
+#include <stdio.h>
+
+int main()
+{
+    int T;
+    scanf("%d", &T);
+
+    for (int i = 1; i <= T; i++)
+    {
+        int a, b;
+        scanf("%d%d", &a, &b);
+        printf("Case %d: %d\n", i, a + b);
+    }
+    return 0;
+}
