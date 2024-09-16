70ab2181d4992f2bcfbb7054aa4f58592e2198fa
jayeen28 <jayeen6783@gmail.com>
2024-09-16 17:59:34 +0600

    find missing problem solved using frequency array

--- cses/findMissingNum.c
+++ cses/findMissingNum.c
@@ -7,19 +7,20 @@ int main()
 
     int N;
     scanf("%d", &N);
-
-    int nums[N];
+    int freq[N];
 
     for (int i = 0; i < N - 1; i++)
     {
         int in;
         scanf("%d", &in);
-        nums[in - 1] = in;
+        if (i == 0)
+            freq[i - 1] = 0;
+        freq[in - 1] += 1;
     }
 
     for (int i = 0; i < N; i++)
     {
-        if (nums[i] != i + 1)
+        if (freq[i] == 0)
         {
             printf("%d\n", i + 1);
             break;
